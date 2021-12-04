de859555febc9d85dc2ed39f916d40b75473069d
Markus Trenkwalder <trenki2@gmx.net>
2021-12-04 20:31:30 +0100

    Refactoring

--- QLearning/Program.cs
+++ QLearning/Program.cs
@@ -1,31 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Xml.Schema;
 
 namespace QLearning
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Test2();
+            Test();
         }
 
-        private static void Test2()
+        private static void Test()
         {
+            var sw = Stopwatch.StartNew();
             var random = new Random();
 
-            var table = new QTable(720, 3);
-            var learner = new QLearner(table, QAlgorithm.Sarsa);
+            var table = new double[720, 3];
+            var agent = new QAgent(table, QAlgorithm.Sarsa, traces: false);
 
-            learner.Temperature = 1;
-            learner.Alpha = 0.5;
-            learner.Gamma = 0.99;
-            learner.Lambda = 0.5;
+            agent.Alpha = 0.5;
+            agent.Gamma = 0.99;
+            agent.Lambda = 0.5;
 
-            var reward = new QTable(720, 3);
+            var reward = new double[720, 3];
             for (int state = 0; state < 720; state++)
             {
                 var action = random.Next(2);
@@ -34,137 +31,21 @@ namespace QLearning
 
             for (var epoch = 0; epoch < 100; epoch++)
             {
-                learner.ResetState(0);
-                learner.Epsilon = 1 - epoch / 100.0;
-                
-                var action = learner.GetPolicyAction(0);
+                agent.Reset(0);
+                agent.Epsilon = 1 - epoch / 100.0;
+
+                var action = agent.GetPolicyAction(0);
                 var cumulativeReward = 0.0;
 
                 for (var state = 0; state < 720 - 1; state++)
                 {
                     cumulativeReward += reward[state, action];
-                    action = learner.Learn(state, action, reward[state, action], state + 1);
+                    action = agent.Step(state, action, reward[state, action], state + 1);
                 }
 
-                Console.WriteLine($"{cumulativeReward / table.GetStateCount():0.000}");
+                Console.WriteLine($"{cumulativeReward / table.GetLength(0):0.000}");
             }
 
-            Console.ReadLine();
-        }
-
-        private static void Test1()
-        {
-            var sw = Stopwatch.StartNew();
-
-            var random = new Random();
-
-            var table = new QTable(16, 16);
-            var learner = new QLearner(table, QAlgorithm.Sarsa);
-            learner.Random = random;
-
-            var gammaTable = new QTable(1, 4);
-            var gammaLearner = new QLearner(gammaTable, QAlgorithm.Sarsa);
-            gammaLearner.Random = random;
-            gammaLearner.Epsilon = 0.05;
-            gammaLearner.Temperature = 0.5;
-
-            var alphaTable = new QTable(1, 4);
-            var alphaLearner = new QLearner(alphaTable, QAlgorithm.Sarsa);
-            alphaLearner.Random = random;
-            alphaLearner.Epsilon = 0.05;
-            alphaLearner.Temperature = 0.5;
-
-            var epsilonTable = new QTable(1, 4);
-            var epsilonLearner = new QLearner(epsilonTable, QAlgorithm.Sarsa);
-            epsilonLearner.Random = random;
-            epsilonLearner.Epsilon = 0.05;
-            epsilonLearner.Temperature = 0.5;
-
-            var temperatureTable = new QTable(1, 4);
-            var temperatureLearner = new QLearner(temperatureTable, QAlgorithm.Sarsa);
-            temperatureLearner.Random = random;
-            temperatureLearner.Epsilon = 0.05;
-            temperatureLearner.Temperature = 0.5;
-
-            var epochs = 100;
-            var steps = 1000;
-            var epsilon = 0.2;
-            var totalReward = 0.0;
-
-            var gammaAction = 0;
-            var alphaAction = 0;
-            var epsilonAction = 0;
-            var temperatureAction = 0;
-
-            for (var epoch = 0; epoch < epochs; epoch++)
-            {
-                var action = 0;
-                var reward = 0.0;
-                var epochReward = 0.0;
-
-                for (var i = 0; i < steps; i++)
-                {
-                    var state = learner.CurrentState;
-                    var nextState = random.NextDouble() < epsilon ? random.Next(table.GetStateCount()) : state;
-
-                    reward = action == nextState ? 1 : 0;
-                    action = learner.Learn(state, action, reward, nextState);
-
-                    totalReward += reward;
-                    epochReward += reward;
-                }
-
-                gammaAction = alphaLearner.Learn(gammaLearner.CurrentState, gammaAction, epochReward / steps, gammaLearner.CurrentState);
-
-                switch (gammaAction)
-                {
-                    case 0: learner.Gamma = 0.3; break;
-                    case 1: learner.Gamma = 0.7; break;
-                    case 2: learner.Gamma = 0.9; break;
-                    case 3: learner.Gamma = 0.99; break;
-                }
-
-                alphaAction = alphaLearner.Learn(alphaLearner.CurrentState, alphaAction, epochReward / steps, alphaLearner.CurrentState);
-
-                switch (alphaAction)
-                {
-                    case 0: learner.Alpha = 0.01; break;
-                    case 1: learner.Alpha = 0.05; break;
-                    case 2: learner.Alpha = 0.10; break;
-                    case 3: learner.Alpha = 0.15; break;
-                }
-
-                epsilonAction = alphaLearner.Learn(epsilonLearner.CurrentState, epsilonAction, epochReward / steps, epsilonLearner.CurrentState);
-
-                switch (epsilonAction)
-                {
-                    case 0: learner.Epsilon = 0.01; break;
-                    case 1: learner.Epsilon = 0.03; break;
-                    case 2: learner.Epsilon = 0.05; break;
-                    case 3: learner.Epsilon = 0.08; break;
-                }
-
-                temperatureAction = temperatureLearner.Learn(temperatureLearner.CurrentState, temperatureAction, epochReward / steps, temperatureLearner.CurrentState);
-
-                switch (temperatureAction)
-                {
-                    case 0: learner.Temperature = 0.1; break;
-                    case 1: learner.Temperature = 0.05; break;
-                    case 2: learner.Temperature = 0.01; break;
-                    case 3: learner.Temperature = 0.005; break;
-                }
-
-                Console.WriteLine($"{epoch}; {gammaAction}; {alphaAction}; {epsilonAction}; {temperatureAction}; {epochReward / steps}");
-
-                //for (var s = 0; s < table.GetStateCount(); s++)
-                //{
-                //    for (var a = 0; a < table.GetActionCount(s); a++)
-                //        Console.Write($"{table[s, a]:0.00}; ");
-                //    Console.WriteLine();
-                //}
-            }
-
-            Console.WriteLine($"{totalReward / (steps * epochs)}");
             Console.WriteLine(sw.ElapsedMilliseconds);
         }
     }
---
+++ QLearning/QAgent.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace QLearning
+{
+    public class QAgent
+    {
+        public bool UseTraces { get; set; }
+        public double[,] QTable { get; set; }
+        public double[,] Traces { get; set; }
+        public QAlgorithm Algorithm { get; set; }
+        public Random Random { get; set; } = new();
+
+        public double Gamma { get; set; } = 0.9;
+        public double Alpha { get; set; } = 0.01;
+        public double Lambda { get; set; } = 0.1;
+        public double Epsilon { get; set; } = 0.01;
+
+        public int CurrentState { get; private set; }
+
+        private readonly int numStates;
+        private readonly int numActions;
+
+        public QAgent(double[,] qtable, QAlgorithm algorithm = QAlgorithm.Q, bool traces = false)
+        {
+            UseTraces = traces;
+            QTable = qtable;
+            Traces = new double[qtable.GetLength(0), qtable.GetLength(1)];
+            Algorithm = algorithm;
+            CurrentState = 0;
+
+            numStates = qtable.GetLength(0);
+            numActions = qtable.GetLength(1);
+        }
+
+        public void Reset(int state)
+        {
+            CurrentState = state;
+            if (UseTraces)
+                ResetTraces();
+        }
+
+        public void ResetTraces()
+        {
+            for (var s = 0; s < numStates; s++)
+                for (var a = 0; a < numActions; a++)
+                    Traces[s, a] = 0.0;
+        }
+
+        public int GetGreedyAction(int state)
+        {
+            var bestAction = Random.Next(numActions);
+            var bestQ = QTable[state, bestAction];
+
+            for (int a = 0; a < numActions; a++)
+            {
+                if (QTable[state, a] > bestQ)
+                {
+                    bestQ = QTable[state, a];
+                    bestAction = a;
+                }
+            }
+
+            return bestAction;
+        }
+
+        public int GetPolicyAction(int state)
+        {
+            if (Random.NextDouble() < Epsilon)
+                return Random.Next(numActions);
+            return GetGreedyAction(state);
+        }
+
+        public int Step(int state, int action, double reward, int nextState)
+        {
+            if (state != CurrentState && UseTraces)
+                ResetTraces();
+
+            var greedyAction = GetGreedyAction(nextState);
+            var policyAction = GetPolicyAction(nextState);
+            var updateAction = Algorithm == QAlgorithm.Q ? greedyAction : policyAction;
+            var delta = reward + Gamma * QTable[nextState, updateAction] - QTable[state, action];
+
+            if (UseTraces)
+            {
+                Traces[state, action] = 1;
+
+                if (numStates > numActions)
+                {
+                    Parallel.For(0, numStates, s =>
+                    {
+                        for (var a = 0; a < numActions; a++)
+                        {
+                            QTable[s, a] += Alpha * delta * Traces[s, a];
+                            Traces[s, a] = Lambda * Gamma * Traces[s, a];
+                        }
+                    });
+                }
+                else
+                {
+                    Parallel.For(0, numActions, a =>
+                    {
+                        for (var s = 0; s < numStates; a++)
+                        {
+                            QTable[s, a] += Alpha * delta * Traces[s, a];
+                            Traces[s, a] = Lambda * Gamma * Traces[s, a];
+                        }
+                    });
+                }
+            }
+            else
+            {
+                QTable[state, action] += Alpha * delta;
+            }
+
+            CurrentState = nextState;
+            return policyAction;
+        }
+    }
+}
